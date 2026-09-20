e0a3a4741e043caa2450df770830bb7da38b9bd5
eyubkh <eyub.kh@gmail.com>
2026-09-20 10:40:25 +0100

    Add site URL to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://www.finon.app",
 	// The site's CSS is about 6 KB, so inlining it saves a render-blocking request on every page.
 	build: { inlineStylesheets: "always" },
 	vite: {
